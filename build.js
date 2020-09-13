f84f31761f76d6a6fccbe71b5d422fb5c63fe4db
Quin <quin@cheesits456.dev>
2020-09-12 23:27:36 -0700

    Update build.js

--- build.js
+++ build.js
@@ -1,5 +1,13 @@
 const fs = require("fs");
 
+const template = fs.readFileSync("./ReactionPics/index.html.template", "utf8");
 const images = fs.readdirSync("./ReactionPics/images");
 
-console.log(images);
+for (const file of images) {
+	const [name, type] = file.split(".");
+	if (!fs.existsSync(`./${name}`)) {
+		fs.mkdirSync(`./${name}`);
+		fs.copyFileSync(`./ReactionPics/images/${file}`, `./${name}/${file}`);
+		fs.writeFileSync(`./${name}/index.html`, template.replace(/{name}/g, name).replace(/{type}/g, type), "utf8");
+	}
+}
