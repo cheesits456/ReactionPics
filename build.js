f2bcda838864309e75cf826571924c03aa5c656d
Quin <quin@cheesits456.dev>
2020-09-13 01:45:42 -0700

    Update build.js

--- build.js
+++ build.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const template = fs.readFileSync("./ReactionPics/index.html.template", "utf8");
 const images = fs.readdirSync("./ReactionPics/images");
 
+fs.copyFileSync("./ReactionPics/README.md", "./README.md");
 for (const file of images) {
 	const [name, type] = file.split(".");
 	if (!fs.existsSync(`./${name}`)) {
